76313c92941640b017f97094fcc25f231b8bfea4
Gr3yG00se64 <Gr3yG00se@protonmail.com>
2019-11-19 07:44:09 -0500

    Everything is operational

---
+++ src/components/AlertInfo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card text-white bg-danger mb-3" style="max-width: 20rem;">
+    <div class="card-header">Severity: {{ severity }}</div>
+    <div class="card-body">
+      <h4 class="card-title">{{ modID }}</h4>
+      <p class="card-text">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AlertInfo",
+  props: ["modID", "description", "severity"]
+};
+</script>
--- src/views/Logger.vue
+++ src/views/Logger.vue
@@ -1,3 +1,48 @@
 <template>
-  <h1>Logger Module Page</h1>
+  <div class="text-center">
+    <h1 class="my-3">Logger Module</h1>
+
+    <p>Longer Description for the Logger Module Here.</p>
+
+    <p>Please press the button to receive the network logs</p>
+
+    <button
+      @click="receiveLog"
+      type="Export Logs"
+      class="btn btn-primary btn-lg"
+    >
+      Download Logs
+    </button>
+  </div>
 </template>
+
+<script>
+import axios from "axios";
+
+const API_URL = "http://localhost:5670/get-logs";
+
+export default {
+  name: "logger",
+  data: function() {
+    return {
+      toggle: false
+    };
+  },
+  methods: {
+    receiveLog() {
+      axios({
+        url: API_URL,
+        method: "GET",
+        responseType: "blob" // important
+      }).then(response => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "log.zip"); //or any other extension
+        document.body.appendChild(link);
+        link.click();
+      });
+    }
+  }
+};
+</script>
